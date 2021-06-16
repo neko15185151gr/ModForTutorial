c4ccc34f503a06b5e599c84afdfd9be61a28ea56
fchb1239 <fchb1239.dk@gmail.com>
2021-06-16 19:48:21 +0200

    Add files via upload

---
+++ ModForTutorial/ModForTutorial/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using BepInEx;
+using UnityEngine;
+using System.Reflection;
+using UnityEngine.XR;
+using Photon.Pun;
+using System.IO;
+using System.Net;
+using Photon.Realtime;
+using UnityEngine.Rendering;
+
+namespace ModForTutorial
+{
+    [BepInPlugin("org.fchb1239.monkeytag.RightTriggerNoGrav", "Right Trigger No Grav", "0.0.6.9")]
+    public class MyPatcher : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            var harmony = new Harmony("com.fchb1239.monkeytag.RightTriggerNoGrav");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(GorillaLocomotion.Player))]
+    [HarmonyPatch("Update", MethodType.Normal)]
+    public class Class1
+    {
+        static bool noGrav = false;
+        static void Postfix(GorillaLocomotion.Player __instance)
+        {
+            if(!PhotonNetwork.CurrentRoom.IsVisible || !PhotonNetwork.InRoom)
+            {
+                List<InputDevice> list = new List<InputDevice>();
+                InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Right | UnityEngine.XR.InputDeviceCharacteristics.Controller, list);
+                list[0].TryGetFeatureValue(CommonUsages.triggerButton, out noGrav);
+
+                if(noGrav)
+                {
+                    __instance.bodyCollider.attachedRigidbody.useGravity = false;
+                }
+                else
+                {
+                    __instance.bodyCollider.attachedRigidbody.useGravity = true;
+                }
+            }
+        }
+    }
+}
